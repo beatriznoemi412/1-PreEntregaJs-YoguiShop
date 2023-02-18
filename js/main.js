4c9807d77c4e5e6a2f431233193a912902f86d98
Beatriz Muñoz <15beatriznoemi@gmail.com>
2023-02-18 20:34:48 -0300

    carrito c ompras

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-let age = prompt("Cuántos años tienes?");
+let age = prompt("Cuántos años tienes?")
 
 const mayoriaEdad = 18;
 
 if (age >= mayoriaEdad) {
-  alert("Puedes comprar");
+  alert("Puedes comprar")
 } else {
-  alert("Regresa cuando tengas 18 años");
+  alert("Regresa cuando tengas 18 años")
 }
 
 //Cálculo de precio final productos con IVA y gasto de envío
@@ -26,35 +26,15 @@ let precioAlmohadon = 7600;
 let gastoEnvio = 600;
 
 let precioFinalRodillo = suma(precioRodillo, iva(precioRodillo), gastoEnvio);
-let precioFinalEsterilla = suma(
-  precioEsterilla,
-  iva(precioEsterilla),
-  gastoEnvio
-);
+let precioFinalEsterilla = suma( precioEsterilla,iva(precioEsterilla),gastoEnvio);
 let precioFinalMat = suma(precioMat, iva(precioMat), gastoEnvio);
 let precioFinalBloque = suma(precioBloque, iva(precioBloque), gastoEnvio);
-let precioFinalAlmohadaLumbar = suma(
-  precioAlmohadaLumbar,
-  iva(precioAlmohadaLumbar),
-  gastoEnvio
-);
+let precioFinalAlmohadaLumbar = suma(precioAlmohadaLumbar,iva(precioAlmohadaLumbar),gastoEnvio);
 let precioFinalKit = suma(precioKit, iva(precioKit), gastoEnvio);
 let precioFinalKitRelax = suma(precioKitRelax, iva(precioKitRelax), gastoEnvio);
-let precioFinalMediasAntideslizantes = suma(
-  precioMediasAntideslizantes,
-  iva(precioMediasAntideslizantes),
-  gastoEnvio
-);
-let precioFinalBolsaYoga = suma(
-  precioBolsaYoga,
-  iva(precioBolsaYoga),
-  gastoEnvio
-);
-let precioFinalAlmohadon = suma(
-  precioAlmohadon,
-  iva(precioAlmohadon),
-  gastoEnvio
-);
+let precioFinalMediasAntideslizantes = suma(precioMediasAntideslizantes,iva(precioMediasAntideslizantes),gastoEnvio);
+let precioFinalBolsaYoga = suma(precioBolsaYoga,iva(precioBolsaYoga),gastoEnvio);
+let precioFinalAlmohadon = suma(precioAlmohadon,iva(precioAlmohadon),gastoEnvio);
 
 //Cálculo de precio final de producto de Oferta incluyendo gasto de envio e iva
 const resta = (a, b) => a - b;
@@ -73,12 +53,12 @@ alert(
 );
 
 //El usuario elige producto y se informa precio final
-let productoElegido = prompt("Hola, desea comprar algún producto: si o no");
+let productoElegido = prompt("Hola, desea comprar algún producto: si o no")
 // "Que producto desea comprar? 1) Rodillo 2) Esterilla 3) Mat 4) Bloque 5) AlmohadaLumbar 6) KitRelax 7) MediasAntideslizantes 8) BolsaYoga 9) Almohadon 10) Oferta"
 
 while (productoElegido != "si" && productoElegido != "no") {
-  alert("por favor ingrese si o no");
-  productoElegido = prompt("Hola, desea comprar algún producto: si o no");
+  alert("por favor ingrese si o no")
+  productoElegido = prompt("Hola, desea comprar algún producto: si o no")
   // "Que producto desea comprar? 1) Rodillo 2) Esterilla 3) Mat 4) Bloque 5) AlmohadaLumbar 6) KitRelax 7) MediasAntideslizantes 8) BolsaYoga 9) Almohadon 10) Oferta")
 }
 if (productoElegido == "si") {
@@ -127,83 +107,89 @@ if (productoElegido == "si") {
   }
 
 } else if(productoElegido == "no"){
-  alert("Gracias por visitarnos!! Hasta pronto.");
+  alert("Gracias por visitarnos!! Hasta pronto.")
 
 }
-while (productoElegido != "no") {
-  let producto = prompt("agrega un producto a tu carrito");
-  let precio = 0;
-
-  if (
-    producto == "Rodillo" ||
-    producto == "Esterilla" ||
-    producto == "Mat" ||
-    producto == "Bloque" ||
-    producto == "AlmohadaLumbar" ||
-    producto == "Kit" ||
-    producto == "KitRelax" ||
-    producto == "MediasAntideslizantes" ||
-    producto == "BolsaYoga" ||
-    producto == "Almohadon"
-  ) {
-    switch (producto) {
-      case "Rodillo":
-        precio = "6529";
-        break;
-
-      case "Esterilla":
-        precio = "5438,79";
-        break;
-
-      case "Mat":
-        precio = "7134";
-        break;
-
-      case "Bloque":
-        precio = "4834";
-        break;
-      case "AlmohadaLumbar":
-        precio = "6287";
-        break;
-      case "Oferta":
-        precio = "13920";
-        break;
-      case "KitRelax":
-        precio = "7739";
-        break;
-      case "MediasAntideslizantes":
-        precio = "5400";
-        break;
-      case "BolsaYoga":
-        precio = "17417";
-        break;
-      case "Almohadon":
-        precio = "9796";
-        break;
-      default:
-        alert:("No tenemos ese producto");
-        break;
-    }
+  while (productoElegido != "no") {
+    let producto = prompt("agrega un producto a tu carrito")
+    let precio = 0
   
-    let unidades = parseInt(prompt("Cuántas unidades quieres?"));
-      if (producto == producto) {
-        let calculo = precio * unidades;
-        console.log(calculo);
-      } else if (producto != producto) {
-        calculo = precio + precio;
-        console.log(calculo);
-        
-      } else {
-        alert("No tenemos ese producto");
+    if (
+      producto == "Rodillo" ||
+      producto == "Esterilla" ||
+      producto == "Mat" ||
+      producto == "Bloque" ||
+      producto == "AlmohadaLumbar" ||
+      producto == "Kit" ||
+      producto == "KitRelax" ||
+      producto == "MediasAntideslizantes" ||
+      producto == "BolsaYoga" ||
+      producto == "Almohadon"
+    ) {
+      switch (producto) {
+        case "Rodillo":
+          precio = "6529";
+          break;
+  
+        case "Esterilla":
+          precio = "5438";
+          break;
+  
+        case "Mat":
+          precio = "7134";
+          break;
+  
+        case "Bloque":
+          precio = "4834";
+          break;
+        case "AlmohadaLumbar":
+          precio = "6287";
+          break;
+        case "Kit":
+          precio = "13920";
+          break;
+        case "KitRelax":
+          precio = "7739";
+          break;
+        case "MediasAntideslizantes":
+          precio = "5400";
+          break;
+        case "BolsaYoga":
+          precio = "17417";
+          break;
+        case "Almohadon":
+          precio = "9796";
+          break;
+        default:
+          alert:("No tenemos ese producto");
+          break;
       }
-  productoElegido = prompt("Quieres seguir comprando?");
+
     
+      let unidades = parseInt(prompt("Cuántas unidades quieres?"));
+      if (producto == producto) {
+      let calculo = precio * unidades
+      console.log(calculo);
+      
+      }
+       else {
+        alert("No tenemos ese producto")
+      }
+  productoElegido = prompt("Quieres seguir comprando?")
   
   while (productoElegido === "no") {
-    alert("gracias por su compra!! Hasta pronto");
-   break;
+    alert("gracias por su compra!! Hasta pronto")
+    break;
   }
+
   
-      }
-    
-    }
+/*let sumarCompra;
+  if (producto != producto){
+    let resultadoTotal = precio + precio
+    console.log(resultadoTotal)
+  }
+  else{
+    alert("no")
+  }*/
+}
+  }
