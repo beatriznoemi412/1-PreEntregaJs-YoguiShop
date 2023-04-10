3a952a8fb36638b8ba206df952bee96e74b557ab
Beatriz Muñoz <15beatriznoemi@gmail.com>
2023-04-10 11:49:24 -0300

    validacion inputs

--- js/compra.js
+++ js/compra.js
@@ -11,57 +11,58 @@ const cvv = document.getElementById("codigoSeguridad");
 const submit = document.getElementById("submit");
 const formulario = document.getElementById("formulario");
 
+numeroTarjeta.addEventListener("input", () => {
+  if (!isNaN(numeroTarjeta.value)) {
+    cardNumber.innerText = numeroTarjeta.value;
+  } else {
+    cardNumber.innerText = "0000 0000 0000 0000";
+  }
+});
+
+nombreTarjeta.addEventListener("input", () => {
+  if (nombreTarjeta.value) {
+    cardName.innerText = nombreTarjeta.value;
+  } else {
+    cardName.innerText = "Juan Perez";
+  }
+});
+mes.addEventListener("input", () => {
+  if (mes.value.length === 2) {
+    cardMonth.innerText = mes.value;
+  } else {
+    cardMonth.innerText = "00";
+  }
+});
+anio.addEventListener("input", () => {
+  if (anio.value.length === 2) {
+    cardYear.innerText = anio.value;
+  } else {
+    cardYear.innerText = "00";
+  }
+});
+
+cvv.addEventListener("input", () => {
+  if (cvv.value.length === 3) {
+    spanCvv.innerText = cvv.value;
+  } else {
+    spanCvv.innerText = "000";
+  }
+});
+//Se evita repetir código y se simplifica la validación de los inputs. Además, si en el futuro se necesita agregar más validaciones, basta con modificar la función validarInput y llamarla para el nuevo input.
+function validarInput(input, mensajeError) {
+  if (!input.value) {
+    throw new Error(mensajeError);
+  }
+}
 function validarCliente(e) {
   e.preventDefault();
 
   try {
-    if (
-      !numeroTarjeta.value ||
-      !nombreTarjeta.value ||
-      !mes.value ||
-      !anio.value ||
-      !cvv.value
-    ) {
-      throw new Error("Por favor, complete todos los campos.");
-    }
-
-    numeroTarjeta.addEventListener("input", () => {
-      if (!isNaN(numeroTarjeta.value)) {
-        cardNumber.innerText = numeroTarjeta.value;
-      } else {
-        cardNumber.innerText = "0000 0000 0000 0000";
-      }
-    });
-
-    nombreTarjeta.addEventListener("input", () => {
-      if (nombreTarjeta.value) {
-        cardName.innerText = nombreTarjeta.value;
-      } else {
-        cardName.innerText = "Juan Perez";
-      }
-    });
-    mes.addEventListener("input", () => {
-      if (mes.value.length === 2) {
-        cardMonth.innerText = mes.value;
-      } else {
-        cardMonth.innerText = "00";
-      }
-    });
-    anio.addEventListener("input", () => {
-      if (anio.value.length === 2) {
-        cardYear.innerText = anio.value;
-      } else {
-        cardYear.innerText = "00";
-      }
-    });
-
-    cvv.addEventListener("input", () => {
-      if (cvv.value.length === 3) {
-        spanCvv.innerText = cvv.value;
-      } else {
-        spanCvv.innerText = "000";
-      }
-    });
+    validarInput(numeroTarjeta, "Por favor, ingrese el número de tarjeta.");
+    validarInput(nombreTarjeta, "Por favor, ingrese el nombre del titular.");
+    validarInput(mes, "Por favor, seleccione el mes de vencimiento.");
+    validarInput(anio, "Por favor, seleccione el año de vencimiento.");
+    validarInput(cvv, "Por favor, ingrese el código de seguridad.");
 
     // Actualiza la información de la tarjeta de crédito en localStorage
     const creditCardInfo = {
@@ -69,20 +70,21 @@ function validarCliente(e) {
       nombreTarjeta: nombreTarjeta.value,
     };
     localStorage.setItem("creditCardInfo", JSON.stringify(creditCardInfo));
-   //Muestra informacion tarjeta cred. en local
-    if (localStorage.getItem("creditCardInfo")) {
-      const creditCardInfo = JSON.parse(localStorage.getItem("creditCardInfo"));
-      numeroTarjeta.value = creditCardInfo.numeroTarjeta;
-      nombreTarjeta.value = creditCardInfo.nombreTarjeta;
-    }
-    
-  // mostrar un mensaje de confirmación utilizando Toastify
+    //Muestra informacion tarjeta cred. en local
+
+    const storedCreditCardInfo = JSON.parse(
+      localStorage.getItem("creditCardInfo")
+    );
+    numeroTarjeta.value = storedCreditCardInfo.numeroTarjeta;
+    nombreTarjeta.value = storedCreditCardInfo.nombreTarjeta;
+
+    // mostrar un mensaje de confirmación utilizando Toastify
     Toastify({
       text: `Compra realizada con éxito.\n¡Gracias por elegirnos!`,
       duration: 4000, // duración del mensaje en milisegundos
       newWindow: true,
       close: true,
-      gravity: "top",
+      gravity: "center",
       position: "center",
       backgroundColor: "linear-gradient(to right, #44C59B , #29AB81)",
       stopOnFocus: true,
@@ -95,7 +97,7 @@ function validarCliente(e) {
       duration: 3000,
       newWindow: true,
       close: true,
-      gravity: "top",
+      gravity: "center",
       position: "center",
       backgroundColor: "linear-gradient(to right, #ff6a00 , #ee0979)",
       stopOnFocus: true,
@@ -104,11 +106,6 @@ function validarCliente(e) {
   }
 }
 
-// resetea la tarjeta de credito cuando form es reseteado
-formulario.addEventListener("reset", () => {
-  resetCamposTarjetaCredito();
-});
-
 function resetCamposTarjetaCredito() {
   cardNumber.innerText = "0000 0000 0000 0000";
   cardName.innerText = "Juan Perez";
@@ -116,11 +113,11 @@ function resetCamposTarjetaCredito() {
   cardYear.innerText = "00";
   spanCvv.innerText = "000";
 }
-
+// resetea la tarjeta de credito cuando form es reseteado
+formulario.addEventListener("reset", () => {
+  resetCamposTarjetaCredito();
+});
+//Evento que valida form al hacer click en botón submit
 submit.addEventListener("click", validarCliente);
 
-
 formulario.reset();
- 
-
-
